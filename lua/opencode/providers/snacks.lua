d4b1b1d5793d34edfc6d8ad9b1f8455d1222991d
Nick van Dyke <nickjvandyke15@gmail.com>
2025-10-24 22:06:34 -0600

    move snacks provider to snacks.Opts since it's just configuration, not the provider itself

--- lua/opencode/providers/snacks.lua
+++ lua/opencode/providers/snacks.lua
@@ -1,17 +1,17 @@
----@class opencode.providers.Snacks : { name: "snacks" }, opencode.providers.Opts, snacks.terminal.Opts
+---@class opencode.providers.snacks.Opts : { name: "snacks" }, opencode.providers.Opts, snacks.terminal.Opts
 
 ---@type opencode.Provider
 return {
-  ---@param opts opencode.providers.Snacks
+  ---@param opts opencode.providers.snacks.Opts
   toggle = function(opts)
     require("snacks.terminal").toggle(opts.cmd, opts)
   end,
-  ---@param opts opencode.providers.Snacks
+  ---@param opts opencode.providers.snacks.Opts
   start = function(opts)
     -- We use `get`, not `open`, so that `toggle` will reference the same terminal
     require("snacks.terminal").get(opts.cmd, opts)
   end,
-  ---@param opts opencode.providers.Snacks
+  ---@param opts opencode.providers.snacks.Opts
   show = function(opts)
     -- Note it only shows if the terminal already exists
     local win = require("snacks.terminal").get(opts.cmd, vim.tbl_deep_extend("force", opts, { create = false }))
